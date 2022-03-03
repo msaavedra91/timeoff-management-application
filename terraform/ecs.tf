1fca028a00cc5fdf04baf3fb9cc15f9f07ac68e7
Miguel Saavedra <miguel.saavedra@6connex.com>
2022-03-03 12:05:25 -0500

    fix container def

--- terraform/ecs.tf
+++ terraform/ecs.tf
@@ -5,7 +5,7 @@ resource "aws_ecs_cluster" "ecs_timeoff" {
 
 resource "aws_ecs_task_definition" "ecs_task_timeoff" {
     family                   = "ecs-task-family-timeoff-app"
-    container_definitions    = file("ecs/container-definitions/container-def.json")
+    container_definitions    = file("container-definitions/container-def.json")
     requires_compatibilities = ["FARGATE"]
     network_mode             = "awsvpc"
     memory                   = var.ecs_task_def_memory
