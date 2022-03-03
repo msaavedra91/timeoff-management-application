b87e2394a66a17de51ba502f7c338fee099a92a7
Miguel Saavedra <miguel.saavedra@6connex.com>
2022-03-02 22:31:34 -0500

    added iam.tf again

--- terraform/ecs.tf
+++ terraform/ecs.tf
@@ -12,24 +12,3 @@ resource "aws_ecs_task_definition" "ecs_task_timeoff" {
     cpu                      = var.ecs_task_def_cpu
     execution_role_arn       = "${aws_iam_role.ecsTaskExecutionRole.arn}"
 }
-
-resource "aws_iam_role" "ecsTaskExecutionRole" {
-    name               = "ecsTaskExecutionRole"
-    assume_role_policy = "${data.aws_iam_policy_document.assume_role_policy.json}"
-}
-
-data "aws_iam_policy_document" "assume_role_policy" {
-    statement {
-        actions = ["sts:AssumeRole"]
-
-        principals {
-            type        = "Service"
-            identifiers = ["ecs-tasks.amazonaws.com"]
-        }
-    }
-}
-
-resource "aws_iam_role_policy_attachment" "ecsTaskExecutionRole_policy" {
-    role       = "${aws_iam_role.ecsTaskExecutionRole.name}"
-    policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
-}
